76b1bdc758b247e181d7321900496e0302a5f2d4
Deivis Contreras Cárdenas <deivipluss@gmail.com>
2025-02-27 17:29:07 +0000

    frontendfix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   output: 'export',  // Para generar build estático
   images: {
-    domains: ['res.cloudinary.com'],
     unoptimized: true  // Necesario para builds estáticos
   },
   trailingSlash: true  // Ayuda con rutas relativas
